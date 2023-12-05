b8916b2cf1167d47f694af2195f246fbfb0a376a
NARENDRA JINKA <NARENDRA JINKA@LAPTOP-M73EGCUP>
2023-12-05 09:56:48 +0530

    wtf maven

---
+++ src/main/java/ccc/abcabc/App.java
@@ -0,0 +1,13 @@
+package ccc.abcabc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
